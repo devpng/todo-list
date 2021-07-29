924fcadf77698550c5d5c31daa15a617a4bf47b8
Rafael Desuyo Jr <paeng.alpha@gmail.com>
2021-07-30 00:48:18 +0800

    todo-list with scss implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/_global.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="todo-form-wrapper">
+        <div className="form-header">
+          <h4>Todo List Title</h4>
+          <p>Start writing and manage your schedule</p>
+        </div>
+        <div className="todo-list-wrapper">
+          <div className="todo-add">
+            <input type="text" placeholder="Enter a todo" className="todo-add_input" />
+            <button className="todo-add_btn">Add Todo</button>
+          </div>
+          <ul className="todo-list">
+            <li className="todo">
+              <div className="todo_text">
+                <input type="checkbox" className="todo_text-checkbox" />
+                <p>asdasdas</p>
+              </div>
+              <div className="todo_time">
+                <p>11:30 am</p>
+              </div>
+            </li>
+            <li className="todo">
+              <div className="todo_text">
+                <input type="checkbox" className="todo_text-checkbox" />
+                <p>lorem ipsum</p>
+              </div>
+              <div className="todo_time">
+                <p>12:30 pm</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
